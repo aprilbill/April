d0a7739340bdd9d7bb785957bd06988d4c40bd51
William-Heyman <305196994@qq.com>
2017-05-28 23:57:35 +0800

    1111

--- OPSMain-20170219/OPSMain-小轮转-做4休2/Run Main/frmLoginForm.Designer.vb
+++ OPSMain-20170219/OPSMain-小轮转-做4休2/Run Main/frmLoginForm.Designer.vb
@@ -91,9 +91,9 @@ Partial Class frmLoginForm
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(60, 5)
+        Me.Button1.Location = New System.Drawing.Point(62, 34)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(75, 64)
         Me.Button1.TabIndex = 7
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
