777569a08d9aa77491e3fb35a9e588ff0b34bdf6
aprilbill <1210720@tongji.edu.cn>
2017-05-29 15:20:51 +0800

    new database

--- OPSMain-20170219/OPSMain-小轮转-做4休2/Run Main/frmLoginForm.Designer.vb
+++ OPSMain-20170219/OPSMain-小轮转-做4休2/Run Main/frmLoginForm.Designer.vb
@@ -32,7 +32,6 @@ Partial Class frmLoginForm
         Me.OK = New System.Windows.Forms.Button()
         Me.Cancel = New System.Windows.Forms.Button()
         Me.cmbUser = New System.Windows.Forms.ComboBox()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'UsernameLabel
@@ -89,15 +88,6 @@ Partial Class frmLoginForm
         Me.cmbUser.TabIndex = 6
         Me.cmbUser.Text = "admin"
         '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(62, 34)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 64)
-        Me.Button1.TabIndex = 7
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
         'frmLoginForm
         '
         Me.AcceptButton = Me.OK
@@ -105,7 +95,6 @@ Partial Class frmLoginForm
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.Cancel
         Me.ClientSize = New System.Drawing.Size(256, 144)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.cmbUser)
         Me.Controls.Add(Me.Cancel)
         Me.Controls.Add(Me.OK)
@@ -124,6 +113,5 @@ Partial Class frmLoginForm
 
     End Sub
     Friend WithEvents cmbUser As System.Windows.Forms.ComboBox
-    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
